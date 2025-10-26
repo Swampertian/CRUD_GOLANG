6dff323f4929d26a73e5e22b62c2c237155b6c3a
Pedro <pedrolmoreira99@gmail.com>
2025-10-25 22:53:12 -0300

    Add Domain layer

---
+++ src/model/user_domain_interface.go
@@ -0,0 +1,42 @@
+package model
+
+type UserDomainInterface interface {
+	GetEmail() string
+	GetPassword() string
+	GetAge() int8
+	GetName() string
+	GetID() string
+	SetID(string)
+	EncryptPassword()
+}
+
+func NewUserDomain(
+	email, password, name string,
+	age int8,
+) UserDomainInterface {
+	return &userDomain{
+		email:    email,
+		password: password,
+		name:     name,
+		age:      age,
+	}
+}
+
+func NewUserLoginDomain(
+	email, password string,
+) UserDomainInterface {
+	return &userDomain{
+		email:    email,
+		password: password,
+	}
+}
+
+func NewUserUpdateDomain(
+	name string,
+	age int8,
+) UserDomainInterface {
+	return &userDomain{
+		name: name,
+		age:  age,
+	}
+}
