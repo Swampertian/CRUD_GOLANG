b60035c08d345ecac4e28baa16184ed2215e66ed
Pedro <pedrolmoreira99@gmail.com>
2025-10-26 01:36:41 -0300

    Adicionar estrutura UserLogin e atualizar rotas para incluir login de usuário

--- src/controller/routes/routes.go
+++ src/controller/routes/routes.go
@@ -2,13 +2,16 @@ package routes
 
 import (
 	"github.com/Swampertian/CRUD_GOLANG/src/controller"
+	"github.com/Swampertian/CRUD_GOLANG/src/model"
 	"github.com/gin-gonic/gin"
 )
 
-func InitRoutes(r *gin.RouterGroup) {
-	r.GET("/getUserById/:userID", controller.FindUserByID)
-	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
-	r.POST("/createUser", controller.CreateUser)
-	r.PUT("/updateUser/:userID", controller.UpdateUser)
-	r.DELETE("/deleteUser/:userID", controller.DeleteUser)
+func InitRoutes(r *gin.RouterGroup, userController controller.UserControllerInterface) {
+
+	r.GET("/getUserById/:userId", model.VerifyTokenMiddleware, userController.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", model.VerifyTokenMiddleware, userController.FindUserByEmail)
+	r.POST("/createUser", userController.CreateUser)
+	r.PUT("/updateUser/:userId", model.VerifyTokenMiddleware, userController.UpdateUser)
+	r.DELETE("/deleteUser/:userId", model.VerifyTokenMiddleware, userController.DeleteUser)
+	r.POST("/login", userController.LoginUser)
 }
