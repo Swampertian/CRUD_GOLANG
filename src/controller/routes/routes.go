9a9d8abaa29f999f70491534c4e05a94077d17a6
Pedro <pedrolmoreira99@gmail.com>
2025-10-25 22:28:40 -0300

    Created routes.go

---
+++ src/controller/routes/routes.go
@@ -0,0 +1,14 @@
+package routes
+
+import (
+	"github.com/Swampertian/CRUD_GOLANG/src/controller"
+	"github.com/gin-gonic/gin"
+)
+
+func InitRoutes(r *gin.RouterGroup) {
+	r.GET("/getUserById/:userID", controller.FindUserByID)
+	r.GET("/getUserByEmail/:userEmail", controller.FindUserByEmail)
+	r.POST("/createUser", controller.CreateUser)
+	r.PUT("/updateUser/:userID", controller.UpdateUser)
+	r.DELETE("/deleteUser/:userID", controller.DeleteUser)
+}
